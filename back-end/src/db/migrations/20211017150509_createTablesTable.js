09b62455f7f2d74fe56b7b4a2e7f289fb7ab4e9c
“Taylor <duggan.taylor24@yahoo.com>
2021-10-17 16:49:18 -0500

    fixing the seeds

--- back-end/src/db/migrations/20211017150509_createTablesTable.js
+++ back-end/src/db/migrations/20211017150509_createTablesTable.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("tables", (table) => {
     table.increments("table_id").primary();
     table.string("table_name").notNullable();
-    table.integer("capacity".notNullable();
+    table.integer("capacity").notNullable();
     table
       .foreign("reservation_id")
       .references("reservation_id")
