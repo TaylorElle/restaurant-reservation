58542dd618642280327483075cf5246ce2274c65
“Taylor <duggan.taylor24@yahoo.com>
2021-10-19 12:39:39 -0500

    working on user story 6

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -3,36 +3,35 @@ const knex = require("../db/connection");
 const tableName = "reservations";
 
 function list(date) {
-  return knex("reservations")
+  return knex(tableName)
     .select("*")
     .where({ reservation_date: date })
-    .andWhereNot({ status: "finished" })
+    .whereNotIn("status", ["finished", "cancelled"])
     .orderBy("reservation_time");
 }
 
 function create(reservation) {
-  return knex("reservations")
+  return knex(tableName)
     .insert(reservation, "*")
     .then((createdRecords) => createdRecords[0]);
 }
 
 function read(reservationId) {
-  return knex("reservations")
-    .select("*")
+  return knex(tableName)
     .where({ reservation_id: reservationId })
     .then((records) => records[0]);
-}
-function update(updatedReservation) {
-  return knex("reservations")
-    .select("*")
-    .where({ reservation_id: updatedReservation.reservation_id })
-    .update(updatedReservation, "*")
-    .then((updatedReservations) => updatedReservations[0]);
 }
 
 function status(reservation) {
   update(reservation);
   return validStatus(reservation);
+}
+
+function update(updatedReservation) {
+  return knex(tableName)
+    .where({ reservation_id: updatedReservation.reservation_id })
+    .update(updatedReservation, "*")
+    .then((updatedReservations) => updatedReservations[0]);
 }
 
 function validStatus(reservation) {
