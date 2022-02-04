61a1deaa04645ec7a196994ca7cbcbc81019e925
“Taylor <duggan.taylor24@yahoo.com>
2022-02-03 18:32:45 -0600

    removing auth0

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,25 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="dev-24nq1xu0.us.auth0.com"
-    clientId="CazVSWFOLsHiVF3Qb9piaCIcmvkmpOTR"
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+
   document.getElementById("root")
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Router>
-//       <App />
-//     </Router>
-//   </React.StrictMode>,
-
-//   document.getElementById("root")
-// );
